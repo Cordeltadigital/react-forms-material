fe8a1632abccd57055c23c0c94ab1e8b010dcc3c
Dale Anderson <danderson00@gmail.com>
2019-09-05 00:41:16 +1000

    Refactor to JS. Style radios.

--- src/fields.js
+++ src/fields.js
@@ -5,7 +5,7 @@ import * as core from '@material-ui/core'
 export { Form } from '@cordelta/react-forms'
 export const Submit = wrapSubmit(core.Button)
 
-const base = component => wrapInput(({ className, error, hiddenLabel, variant, label, helperText, ...inputProps }) => (
+const base = (component, options = {}, { alwaysShrinkLabel, shrinkLabel = true } = {}) => wrapInput(({ className, error, hiddenLabel, variant, label, helperText, ...inputProps }) => (
   createElement(core.FormControl, {
     className,
     error,
@@ -14,23 +14,35 @@ const base = component => wrapInput(({ className, error, hiddenLabel, variant, l
     required: inputProps.required,
     variant,
     children: [
-      ...(label ? [createElement(core.InputLabel, { key: 'label', htmlFor: inputProps.id, children: label })] : []),
+      ...(label ? [createElement(core.InputLabel, {
+        key: 'label',
+        htmlFor: inputProps.id,
+        children: label,
+        ...(shrinkLabel === false && { shrink: false }),
+        ...(alwaysShrinkLabel && { className: 'MuiInputLabel-shrink' }),
+      })] : []),
+
       createElement(component, { key: 'input', ...inputProps }),
-      ...(helperText ? [createElement(core.FormHelperText, { key: 'helperText', children: helperText })] : [])
+
+      ...(helperText ? [createElement(core.FormHelperText, {
+        key: 'helperText',
+        children: helperText
+      })] : [])
     ]
   })
-), { passErrorProp: true })
+), { passErrorProp: true, ...options })
 
 export const Text = base(core.Input)
-export const Checkbox = base(core.Checkbox)
-export const RadioButton = base(core.Radio)
+export const Checkbox = base(core.Checkbox, { type: 'checkbox' })
+export const RadioButton = base(core.Radio, { type: 'radio' }, { shrinkLabel: false })
 
-export const Radio = base(({ values, labels, ...props }) =>
-  createElement(core.RadioGroup, { children: [
+export const Radio = base(({ name, values, labels, ...props }) =>
+  createElement(core.RadioGroup, { ...props, children: [
     values.map((value, index) =>
-      createElement(RadioButton, { ...props, value, key: value, label: (labels && labels[index]) || value })
+      createElement(RadioButton, { name, value, key: value, label: (labels && labels[index]) || value })
     )
-  ] })
+  ] }),
+  {}, { alwaysShrinkLabel: true }
 )
 
 export const Select = base(({ values, labels, ...props }) =>
@@ -38,5 +50,6 @@ export const Select = base(({ values, labels, ...props }) =>
     values.map((value, index) =>
       createElement(core.MenuItem, { value, key: value, children: (labels && labels[index]) || value })
     )
-  })
+  }),
+  { type: 'select' }
 )
