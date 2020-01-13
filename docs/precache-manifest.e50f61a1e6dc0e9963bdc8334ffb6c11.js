e406ad09ffa817dfda4c0832188e79f897b99930
Dale Anderson <danderson00@gmail.com>
2020-01-13 16:25:24 +1100

    Update to relative path

---
+++ docs/precache-manifest.e50f61a1e6dc0e9963bdc8334ffb6c11.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68c6d6803450d9b996c11a46e8ce0636",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fbc02e02e7bdbea86838",
+    "url": "./static/js/2.1aa5bc88.chunk.js"
+  },
+  {
+    "revision": "0b6dcdd6a309e8ef1380",
+    "url": "./static/js/main.6aa43a05.chunk.js"
+  },
+  {
+    "revision": "f7384c7c37ce9455ec05",
+    "url": "./static/js/runtime-main.fef6fc98.js"
+  }
+]);
