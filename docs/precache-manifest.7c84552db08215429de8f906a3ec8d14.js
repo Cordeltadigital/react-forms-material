5fa269e74bd2181b13b8cc309b78c62cd875b8d0
Dale Anderson <danderson00@gmail.com>
2020-01-13 16:21:38 +1100

    Build to docs directory

---
+++ docs/precache-manifest.7c84552db08215429de8f906a3ec8d14.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbbe6a00390b3bd9d3095b9d2c4f209e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc02e02e7bdbea86838",
+    "url": "/static/js/2.1aa5bc88.chunk.js"
+  },
+  {
+    "revision": "0b6dcdd6a309e8ef1380",
+    "url": "/static/js/main.6aa43a05.chunk.js"
+  },
+  {
+    "revision": "4f7a02da36b761f1f6cf",
+    "url": "/static/js/runtime-main.85d17eb7.js"
+  }
+]);
